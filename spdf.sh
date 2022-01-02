8f89d4936b8362506001b3c439ee3deaf1e16b00
FizzyWhizbang <marc.levine@gmail.com>
2022-01-02 06:35:58 -0600

    update spdf
    added command line arguments and help so only parts of the script can be run

--- spdf.sh
+++ spdf.sh
@@ -1,7 +1,122 @@
 #!/bin/bash
+
+# Reset
+NC='\033[0m'       # Text Reset
+
+# Regular Colors
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+
+# Bold
+BBlack='\033[1;30m'       # Black
+BRed='\033[1;31m'         # Red
+BGreen='\033[1;32m'       # Green
+BYellow='\033[1;33m'      # Yellow
+BBlue='\033[1;34m'        # Blue
+BPurple='\033[1;35m'      # Purple
+BCyan='\033[1;36m'        # Cyan
+BWhite='\033[1;37m'       # White
+
+# Underline
+UBlack='\033[4;30m'       # Black
+URed='\033[4;31m'         # Red
+UGreen='\033[4;32m'       # Green
+UYellow='\033[4;33m'      # Yellow
+UBlue='\033[4;34m'        # Blue
+UPurple='\033[4;35m'      # Purple
+UCyan='\033[4;36m'        # Cyan
+UWhite='\033[4;37m'       # White
+
+# Background
+On_Black='\033[40m'       # Black
+On_Red='\033[41m'         # Red
+On_Green='\033[42m'       # Green
+On_Yellow='\033[43m'      # Yellow
+On_Blue='\033[44m'        # Blue
+On_Purple='\033[45m'      # Purple
+On_Cyan='\033[46m'        # Cyan
+On_White='\033[47m'       # White
+
+# High Intensity
+IBlack='\033[0;90m'       # Black
+IRed='\033[0;91m'         # Red
+IGreen='\033[0;92m'       # Green
+IYellow='\033[0;93m'      # Yellow
+IBlue='\033[0;94m'        # Blue
+IPurple='\033[0;95m'      # Purple
+ICyan='\033[0;96m'        # Cyan
+IWhite='\033[0;97m'       # White
+
+# Bold High Intensity
+BIBlack='\033[1;90m'      # Black
+BIRed='\033[1;91m'        # Red
+BIGreen='\033[1;92m'      # Green
+BIYellow='\033[1;93m'     # Yellow
+BIBlue='\033[1;94m'       # Blue
+BIPurple='\033[1;95m'     # Purple
+BICyan='\033[1;96m'       # Cyan
+BIWhite='\033[1;97m'      # White
+
+# High Intensity backgrounds
+On_IBlack='\033[0;100m'   # Black
+On_IRed='\033[0;101m'     # Red
+On_IGreen='\033[0;102m'   # Green
+On_IYellow='\033[0;103m'  # Yellow
+On_IBlue='\033[0;104m'    # Blue
+On_IPurple='\033[0;105m'  # Purple
+On_ICyan='\033[0;106m'    # Cyan
+On_IWhite='\033[0;107m'   # White
+
+
 cwd=$(pwd)
 
-output="$1" #new merged filename
+
+
+RUNALL=FALSE
+IMAGES=FALSE
+MKPDF=FALSE
+SCALEPDF=FALSE
+MERGE=FALSE
+RESIZE=FALSE
+while getopts "o:aipsmr" flag
+do
+    case "${flag}" in
+        o) OUTPUT=${OPTARG}
+        ;; 
+
+        a) RUNALL=TRUE
+        ;;
+        i) IMAGES=TRUE
+        ;;
+        p) MKPDF=TRUE
+        ;;
+        s) SCALEPDF=TRUE
+        ;;
+        m) MERGE=TRUE
+        ;;
+        r) RESIZE=TRUE
+        ;;
+        \?) 
+        echo "#####################################################"
+        echo "Usage:"
+        echo "-o output name"
+        echo "-a will run the whole process"
+        echo "-i will create images"
+        echo "-p will make PDFs"
+        echo "-s will scale the PDFs to A4 so they all match in size"
+        echo "-m will merge the pdfs into one final container specified in the -o parameter"
+        echo "-r will use ghostscript to resize the reultant container"
+        echo "#####################################################"
+        exit 1
+        ;;
+    esac
+done
 
 function mkimgs() {
     echo "Creating Images"
@@ -50,31 +165,80 @@ function scalepdf() {
 
 #2480x3508
 function mergepdfs() {
+    echo "Merging PDFs with pdfunite"
     cd pdfscale
-    if [ ${output##*.} == "pdf" ]
+    if [ ${OUTPUT##*.} == "pdf" ]
     then
-        filename=${output##*.}
+        filename=${OUTPUT##*.}
     else
-        filename="$output"
+        filename="${OUTPUT}"
     fi
 
     pdfunite $(ls -v *.pdf) "../$filename.pdf"
+    #shrink pdf
+    if [ $RESIZE == TRUE ]
+    then
+        echo "Resizing PDF with Ghostscript"
+        ghostscript -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/printer -dNOPAUSE -dQUIET -dBATCH -sOutputFile="${filename}_resize.pdf" "$filename.pdf"
+    fi
 }
 
-echo "Creating images directory"
-mkdir images
-i=0
-for f in *.pdf
-do
-mkimgs $f
-echo $i
-((i=i+1))
-done
-echo "Creating PDFs directory"
-mkdir pdf
-mkpdfs
-echo "Scaling pdfs to A4"
-scalepdf
+printf "OUTPUT Name: ${Green} ${OUTPUT} ${NC}\n"
 
-echo "Creating Single PDF from individual files"
-mergepdfs
+
+if [ $RUNALL == TRUE ]
+then
+    echo "Running the whole shebang"
+    echo "Creating images directory"
+    mkdir images
+    i=0
+    for f in *.pdf
+    do
+    mkimgs $f
+    echo $i
+    ((i=i+1))
+    done
+    echo "Creating PDFs directory"
+    mkdir pdf
+    mkpdfs
+    echo "Scaling pdfs to A4"
+    scalepdf
+
+    echo "Creating Single PDF from individual files"
+    RESIZE=TRUE
+    mergepdfs
+    else
+        if [ $IMAGES == TRUE ]
+        then
+            echo "Creating images directory"
+            mkdir images
+            i=0
+            for f in *.pdf
+            do
+            mkimgs $f
+            echo $i
+            ((i=i+1))
+            done
+        fi
+
+        if [ $MKPDF == TRUE ]
+        then
+            echo "Creating PDFs directory"
+            mkdir pdf
+            mkpdfs
+        fi
+        if [ $SCALEPDF == TRUE ]
+        then
+            echo "Scaling pdfs to A4"
+            scalepdf
+        fi
+        if [ $MERGE == TRUE ]
+        then
+            mergepdfs
+        fi
+
+fi
+
+
+
+
