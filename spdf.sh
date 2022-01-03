83ec908a3ab094adf7fc6d3a6e015b9ff519257a
FizzyWhizbang <marc.levine@gmail.com>
2022-01-03 04:30:02 -0600

    Update spdf.sh
    put setting info for ghost script

--- spdf.sh
+++ spdf.sh
@@ -181,6 +181,11 @@ function mergepdfs() {
     then
         cd "$cwd"
         echo "Resizing PDF with Ghostscript"
+        # -dPDFSETTINGS=/screen lower quality, smaller size. (72 dpi)
+        # -dPDFSETTINGS=/ebook for better quality, but slightly larger pdfs. (150 dpi)
+        # -dPDFSETTINGS=/prepress output similar to Acrobat Distiller "Prepress Optimized" setting (300 dpi)
+        # -dPDFSETTINGS=/printer selects output similar to the Acrobat Distiller "Print Optimized" setting (300 dpi)
+        # -dPDFSETTINGS=/default selects output intended to be useful across a wide variety of uses, possibly at the expense of a larger output file
         ghostscript -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/printer -dNOPAUSE -dQUIET -dBATCH -sOutputFile="${filename}_resize.pdf" "$filename.pdf"
     fi
 }
